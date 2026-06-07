56fba8625e024ff3eb841ac8700ef51605bb0eb4
Sebastian Freundt <freundt@ga-group.nl>
2026-06-07 09:08:46 +0000

    fix provenance of icb

--- sql/infer-icb.sql
+++ sql/infer-icb.sql
@@ -351,7 +351,7 @@ PREFIX delta: <http://www.w3.org/2004/delta#>
 WITH <$u{TGTGR}>
 INSERT {
 	?x
-		pav:createdWith <file:infer-gics.sql> ;
+		pav:createdWith <file:infer-icb.sql> ;
 		pav:sourceAccessedOn ?acc1 ;
 		pav:sourceLastAccessedOn ?accl ;
 		tempo:validFrom ?minfrom ;
