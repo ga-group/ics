af6be42276ebaaac1326fdf9ef953a36d741a613
Sebastian Freundt <freundt@ga-group.nl>
2026-06-08 09:30:12 +0000

    provide SPARQL to generate bics hierarchy

---
+++ sql/bicshier.sql
@@ -0,0 +1,13 @@
+SPARQL
+PREFIX bics: <http://data.ga-group.nl/ics/bics/>
+
+SELECT (CONCAT(SUBSTR("                ", 1, STRLEN(?cod) - 2), "- ", STR(?cod), "  ", STR(?lbl), "  ") AS ?r)
+FROM <http://data.ga-group.nl/ics/bics/>
+WHERE {
+	?x a bics:Classifier ;
+		skos:notation ?cod ;
+		rdfs:label ?lbl .
+}
+GROUP BY ?cod ?lbl
+ORDER BY ?cod
+;
